6aa3a7f80874e445295198e46cac23d6bcd8ea28
Aurora Scharff <aurora.sofie@gmail.com>
2026-07-23 14:59:08 +0200

    update structure

--- app/(app)/favorites/page.tsx
+++ app/(app)/favorites/page.tsx
@@ -18,28 +18,21 @@ export default function FavoritesPage() {
       <Suspense fallback={<TrackListSkeleton count={5} showIndex />}>
         <Crossfade>
           <FavoritesFeed />
+          <section>
+            <h2 className="mt-10 mb-4">You Might Also Like</h2>
+            <ErrorBoundary title="Couldn't load recommendations">
+              <Suspense fallback={<DiscoverSkeleton />}>
+                <Discover />
+                <Crossfade>
+                  <section className="mt-10">
+                    <h2 className="mb-4">Explore Genres</h2>
+                    <TopGenresGrid />
+                  </section>
+                </Crossfade>
+              </Suspense>
+            </ErrorBoundary>
+          </section>
         </Crossfade>
-        <ErrorBoundary title="Couldn't load recommendations">
-          <Suspense
-            fallback={
-              <>
-                <h2 className="mt-10 mb-4">You Might Also Like</h2>
-                <DiscoverSkeleton />
-              </>
-            }
-          >
-            <Crossfade>
-              <h2 className="mt-10 mb-4">You Might Also Like</h2>
-              <Discover />
-            </Crossfade>
-            <Crossfade>
-              <section className="mt-10">
-                <h2 className="mb-4">Explore Genres</h2>
-                <TopGenresGrid />
-              </section>
-            </Crossfade>
-          </Suspense>
-        </ErrorBoundary>
       </Suspense>
     </PageWrapper>
   );
