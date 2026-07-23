489df106472ea51738dbd4b5f66b9a8736466a26
Aurora Scharff <aurora.sofie@gmail.com>
2026-07-23 12:06:22 +0200

    improve recommended songs logic

--- features/track/track-queries.ts
+++ features/track/track-queries.ts
@@ -171,7 +171,6 @@ export async function getRecommendedTracks(excludeTrackId: string, limit: number
     where: {
       id: { not: excludeTrackId },
       favorites: { none: { userId } },
-      trackPlays: { none: { userId } },
     },
   });
   return rows.map(row => toTrack(row));
