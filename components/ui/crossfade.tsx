d363be500987a112c22ec1463a82ceac37196784
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-31 20:13:06 +0200

    up

--- components/ui/crossfade.tsx
+++
@@ -1,9 +0,0 @@
-import { ViewTransition } from 'react';
-
-export function Crossfade({ children }: { children: React.ReactNode }) {
-  return (
-    <ViewTransition enter="auto" default="none">
-      {children}
-    </ViewTransition>
-  );
-}
