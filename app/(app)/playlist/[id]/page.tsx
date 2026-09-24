f057cb0569eefbcd4cddfe313dad7d5b572c9495
Aurora Scharff <aurora.sofie@gmail.com>
2026-09-24 12:28:54 +0200

    up

--- app/(app)/playlist/[id]/page.tsx
+++ app/(app)/playlist/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { PageWrapper } from '@/components/ui/page-layout';
 
-export default async function PlaylistDetailPage(_props: PageProps<'/playlist/[id]'>) {
+export default function PlaylistDetailPage(_props: PageProps<'/playlist/[id]'>) {
   return (
     <PageWrapper>
       {/* Playlist detail */}
